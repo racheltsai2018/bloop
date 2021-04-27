77fadc396e1a060aa5b09aa4f37ba7a296ed5219
Jennifer Yang <42123788+jennifury@users.noreply.github.com>
2021-04-27 02:13:20 -0700

    added

---
+++ bloop_app/lib/game_main.dart
@@ -0,0 +1,99 @@
+import 'package:flame/components/component.dart';
+import 'package:flutter/material.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/services.dart';
+import 'package:bloop_app/shooter-game.dart';
+import 'package:flutter/gestures.dart';
+
+
+void main(){
+  WidgetsFlutterBinding.ensureInitialized(); // necessary if you want to do something before runApp
+  Flame.util.fullScreen(); // just makes the game full screen
+
+  runApp(MyApp());
+  /*
+  ShooterGame game = ShooterGame();
+  runApp(game.widget); // this has to come before Until functions because later Flutter updates "broke it"
+
+  Util flameUtil = Util();
+  flameUtil.fullScreen();
+  flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  //recognizing gestures
+  TapGestureRecognizer tapper = TapGestureRecognizer();
+  PanGestureRecognizer pan = PanGestureRecognizer();
+  tapper.onTapDown = game.onTapDown; //call game class tap when gesture is tap
+ //pan.onStart = game.onPanStart;
+  //pan.onEnd = game.onPanEnd;
+  //pan.onUpdate = game.onPanUpdate;
+  runApp(game.widget);
+  flameUtil.addGestureRecognizer(tapper); //figure this out later
+  flameUtil.addGestureRecognizer(pan);
+*/
+}
+
+// Base code that came with flutter
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shooter Game',
+      theme: ThemeData(
+        fontFamily: 'Audiowide',
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Shooter Game'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ShooterGame game;
+
+  @override
+  void initState(){
+    super.initState();
+    game = ShooterGame();
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: game.widget, // I wish I knew how widgets works heheheheheh -Ira
+    );
+  }
+}
