0ea3950160613a8c04a005d5f614ac173c622b8e
Jennifer Yang <42123788+jennifury@users.noreply.github.com>
2021-04-27 02:12:02 -0700

    diary UI

--- bloop_app/lib/diary.dart
+++ bloop_app/lib/diary.dart
@@ -1,13 +1,24 @@
+import 'package:bloop_app/addDiary.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:bloop_test/homepage.dart';
-import 'package:bloop_test/meditation.dart';
 
-class diary extends StatelessWidget {
+
+class diary extends StatefulWidget{
+  @override
+  _diaryState createState() => new _diaryState();
+}
+
+class _diaryState extends State<diary>{
+  List<DiaryEntry> allEntry;
+  int i=0;
+  void main() async{
+    allEntry = await diaries();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: AppBar(                              //bar on top
         title: Text('Diary',
             style: GoogleFonts.fascinate(
                 color: Colors.black, fontSize: 25.0
@@ -28,9 +39,47 @@ class diary extends StatelessWidget {
               Colors.white
             ]
         )),
-
-      ),
-
-    );
+        child: Column(
+          children: <Widget>[
+            Align(                                                //plus button to add diary page
+              alignment: Alignment.topRight,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(8.0, 8.0, 0, 8.0),
+                child: RawMaterialButton(
+                  elevation: 5.0,
+                  fillColor: Colors.grey,
+                  child: Icon(
+                    Icons.add,
+                    size: 30.0,
+                  ),
+                  padding: EdgeInsets.all(15.0),
+                  shape: CircleBorder(),
+                  onPressed: (){
+                    Navigator.push(context, MaterialPageRoute<void>(builder: (context) => addDiary()));
+                  },
+                ),
+              ),
+            ),
+            FutureBuilder<List>(                                              //get diary info
+                future: diaries(),
+                initialData: allEntry,
+                builder: (context, snapshot){
+                  if(snapshot.hasData){
+                    return Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Text('Diary date: ${snapshot.data[i]['date']}'),
+                            Text('Mood: ${snapshot.data[i]['emoji']}'),
+                          ],
+                        )
+                    );
+                  }else{
+                    return Text("Currently no data entry");
+                  }
+                }
+            ),
+          ],
+        ),
+      ),);
   }
 }
